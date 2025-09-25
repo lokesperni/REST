d7028d965056cbee65094fc07dea61a67ddc1492
lokesperni <lokeslokeswarao@gmail.com>
2025-09-25 18:59:30 +0530

    added the code

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,28 +1,45 @@
-var book = require("../model/book")
+var express = require("express")
+
+var Book = require("../model/Book")
 
 var getAllBooks = async(req,res)=>{
-    var allbooks = await book.find()
-    res.status(200).json(allbooks)
+    var allBooks = await Books.find()
+    res.status(200).json(allBooks)
 }
 
 var getSingleBook = async(req,res)=>{
-
+    var BookId = req.params.id
+    var getSingleBook = await Books.findById(BookId)
+    res.status(200).json(getSingleBook)
 }
 
 var addNewBook = async(req,res)=>{
-    var newFormBook = req.body
-    var newBook = await book.create(newFormBook)
+    var Bookdata = req.body
+    var newBook = await Books.create(Bookdata)
     if(newBook){
         res.status(201).json(newBook)
+    }else{
+        res.status(404).json({message : "cannot add"})
     }
 }
 
 var updateBook = async(req,res)=>{
-
+    var BookId = req.params.id
+    var updateddata = req.body
+    var updatedBook = await Books.findByIdAndUpdate(BookId,updateddata)
+    if(updatedBook){
+        res.status(200).json(updatedBook)
+    }else{
+        res.status(404).json({message : "cannot find"})
+    }
 }
 
 var deleteBook = async(req,res)=>{
-
+    var BookId = req.params.id
+    var deletedBook = await Books.findByIdAndDelete(BookId)
+    if(deletedBook){
+        res.status(404).json({message : "cannot delete"})
+    }
 }
 
 module.exports = {
