477ebbb35b43e5d35b53ce51dee574c0a33f9bc1
lokesperni <lokeslokeswarao@gmail.com>
2025-09-30 16:48:43 +0530

    added the code

--- routes/product-route.js
+++ routes/product-route.js
@@ -1,13 +1,17 @@
 var express = require("express")
 
-var {getAllproducts,getSingleProduct,addNewProduct,updateNewProduct,deleteProduct} = require("../controllers/product-controller")
+var {getAllproducts, getSingleProduct, addNewProduct, updateNewProduct, deleteProduct} = require("../controllers/product-controller")
 
 var router = express.Router()
 
-router.get("/get",getAllproducts)
-router.get("/get/:id",getSingleProduct)
-router.post("/add",addNewProduct)
-router.put("/update/:id",updateNewProduct)
-router.delete("/delete/:id",deleteProduct)
+router.get("/get", getAllproducts)
+
+router.get("/get/:id", getSingleProduct)
+
+router.post("/add", addNewProduct)
+
+router.put("/update/:id", updateNewProduct)
+
+router.delete("/delete/:id", deleteProduct)
 
 module.exports = router 
